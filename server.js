35878340b6d40bb791a2705867d6c76073f7683b
Vnovnick <safonov.nick10@gmail.com>
2023-10-02 21:01:57 -0700

    alter if statements; test post (not recieving data)

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const app = express();
-// import { pool } from "./dbConfig";
+const bcrypt = require("bcrypt");
+const { pool } = require("./dbConfig");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello" });
+  res.send({ message: "yes" });
 });
 
 app.post("/", (req, res) => {
-  const { username, email, password, confirmPassword } = req.body;
-  console.log({ username, email, password, confirmPassword });
+  // const { username, email, password, confirmPassword } = req.body;
+  console.log(req.body);
+  // console.log({ username, email, password, confirmPassword });
   //   res.send({ username, email, password, confirmPassword })
 
   if (!username || !email || !password || !confirmPassword) {
     res.status(500).send({ message: "Please enter all fields" });
-  }
-
-  if (password && password.length < 6) {
+  } else if (password && password.length < 6) {
     res
       .status(500)
       .send({ message: "Password should be at least 6 characters" });
-  }
-
-  if (password !== confirmPassword) {
+  } else if (password !== confirmPassword) {
     res.status(500).send({ message: "Password do not match" });
+  } else {
+    const hashedPassword = bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    res.status(201).send({ message: "Success" });
   }
 });
 
