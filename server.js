edfec776d9781a8e04da179120cbb869ca5c1753
Vnovnick <safonov.nick10@gmail.com>
2023-10-04 21:32:56 -0700

    comment out validations transferred to frontend

--- server.js
+++ server.js
@@ -15,47 +15,48 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
   const { username, email, password, confirmPassword } = req.body;
 
-  if (!username || !email || !password || !confirmPassword) {
-    res.status(500).send({ message: "Please enter all fields" });
-  } else if (password && password.length < 6) {
-    res
-      .status(500)
-      .send({ message: "Password should be at least 6 characters" });
-  } else if (password !== confirmPassword) {
-    res.status(500).send({ message: "Password do not match" });
-  } else {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  // commenting out validation since its handled in the svelte action
+  // if (!username || !email || !password || !confirmPassword) {
+  //   res.status(500).send({ message: "Please enter all fields" });
+  // } else if (password && password.length < 6) {
+  //   res
+  //     .status(500)
+  //     .send({ message: "Password should be at least 6 characters" });
+  // } else if (password !== confirmPassword) {
+  //   res.status(500).send({ message: "Password do not match" });
+  // } else {
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    pool.query(
-      `SELECT * FROM users
+  pool.query(
+    `SELECT * FROM users
       WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          throw err;
-        }
+    [email],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
 
-        if (results.rows.length > 0) {
-          res.status(500).send({ message: "Email already registered" });
-        } else {
-          pool.query(
-            `INSERT INTO users (username, email, password)
+      if (results.rows.length > 0) {
+        res.status(500).send({ message: "Email already registered" });
+      } else {
+        pool.query(
+          `INSERT INTO users (username, email, password)
             VALUES ($1, $2, $3)
             RETURNING id, password`,
-            [username, email, hashedPassword],
-            (err, results) => {
-              if (err) {
-                throw err;
-              }
-              console.log(results.rows);
-              console.log("new user registered");
-              res.status(201).send({ message: "User Successfully created" });
+          [username, email, hashedPassword],
+          (err, results) => {
+            if (err) {
+              throw err;
             }
-          );
-        }
+            console.log(results.rows);
+            console.log("new user registered");
+            res.status(201).send({ message: "User Successfully created" });
+          }
+        );
       }
-    );
-  }
+    }
+  );
+  // }
 });
 
 app.listen(PORT, () => {
